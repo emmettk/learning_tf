a7dd9483cd5d75af8ed5cb1846184f5eb856b8ce
Emmett Krupczak <eekrupczak@gmail.com>
2017-11-29 15:50:35 -0500

    moving carrot version

--- src/frame_tf_broadcaster.cpp
+++ src/frame_tf_broadcaster.cpp
@@ -14,8 +14,14 @@ int main(int argc, char** argv){
 
   ros::Rate rate(10.0);
   while (node.ok()){
+    
+    //Constant offset version
     //carrot1 frame is 2m offset to left of turtle1 frame
-    transform.setOrigin( tf::Vector3(0.0, 2.0, 0.0) );
+    // transform.setOrigin( tf::Vector3(0.0, 2.0, 0.0) );
+    //transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
+    
+    //Moving offset version
+    transform.setOrigin( tf::Vector3(2.0*sin(ros::Time::now().toSec()), 2.0*cos(ros::Time::now().toSec()), 0.0) );
     transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "turtle1", "carrot1"));
     rate.sleep();
